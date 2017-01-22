a3343704743d153a311d74f0d0cf52c88491ab5b
Patrick Vacek <prvacek@ucdavis.edu>
2017-01-22 11:28:52 -0800

    Update server.R

--- finalfiles/server.R
+++ finalfiles/server.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(RCurl)
 
 elec<-read.csv("https://raw.githubusercontent.com/ThisIsJeron/CEEDR/master/finalfiles/electricity.csv")
+timestamp<-unique(elec[,3])
 
 shinyServer(function(input,output){
     output$plot<-renderPlot({
@@ -12,7 +13,7 @@ shinyServer(function(input,output){
         data<-elec[elec[,3]==timestamp[input$slide],]
         return(data)})
       data<-dataInput()
-      qplot(x=data[,5],y=data[,4],col=data[,6],main=paste0("Hourly Electricity Usage at ",as.character(timestamp[input$slide])))
-      
+      qplot(x=data[,5],y=data[,4],col=data[,7],main=paste0("Hourly Electricity Usage at ",as.character(timestamp[input$slide])),xlab="Longitude",
+      ylab="Latitude")
     })
   })
